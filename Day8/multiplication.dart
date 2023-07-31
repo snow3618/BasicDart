d5bdcd3bf78c3cf774fed94565ef005dd178028a
snow3618 <95566813+snow3618@users.noreply.github.com>
2023-07-31 18:37:11 +0630

    Create multiplication.dart

---
+++ Day8/multiplication.dart
@@ -0,0 +1,13 @@
+/* Multiplication data */
+void main(){
+   int i =1;
+   int result = 0;
+   while( i<11){
+        print('$i multiplication');
+       for(int j=1; j<13; j++){
+        result = i* j;
+        print('$i * $j = $result');           
+       }
+       i++;
+   }
+}
